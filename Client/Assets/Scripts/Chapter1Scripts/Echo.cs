0efa9521b6bf8ab8c3ba2528e580322da4319ce3
Severn <1004005788@qq.com>
2020-04-25 15:20:18 +0800

    网络游戏的开端:Echo

---
+++ Client/Assets/Scripts/Chapter1Scripts/Echo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Echo : MonoBehaviour
+{
+    // 定义套接字
+    private Socket socket;
+
+    public InputField inputfield;
+    public Text text;
+
+    public void Connection()
+    {
+        // socket
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Connect("127.0.0.1",8888);
+    }
+
+    public void Send()
+    {
+        // Send
+        string sendStr = inputfield.text;
+        byte[] sendBytes = Encoding.Default.GetBytes(sendStr);
+        socket.Send(sendBytes);
+        // Recv
+        byte[] readBuff = new byte[1024];
+        int count = socket.Receive(readBuff);
+        string recvStr = Encoding.Default.GetString(readBuff, 0, count);
+        text.text = recvStr;
+        // Close
+        socket.Close();
+    }
+}
---
+++ Server/EchoServer/EchoServer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+
+            // socket
+            Socket listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // Bind
+            IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEd = new IPEndPoint(ipAdr, 8888);
+            listenfd.Bind(ipEd);
+            listenfd.Listen(0);
+            Console.WriteLine("[服务器]启动成功");
+            while (true)
+            {
+                //accept
+                Socket connfd = listenfd.Accept();
+                Console.WriteLine("[服务器]Accept");
+                //Receive
+                byte[] readBuff = new byte[1024];
+                int count = connfd.Receive(readBuff);
+                string readStr = Encoding.Default.GetString(readBuff, 0, count);
+                Console.WriteLine("[服务器接收]" + readStr);
+                //Send
+                byte[] sendBytes = Encoding.Default.GetBytes(readStr);
+                connfd.Send(sendBytes);
+            }
+        }
+        public static void GetTime(string str)
+        {
+            if (!string.IsNullOrEmpty(str))
+            {
+                if (str.Contains("时间"))
+                {
+                    Console.WriteLine("[服务器]当前时间:" + DateTime.Now.ToString());
+                }
+            }
+        }
+    }
+}
