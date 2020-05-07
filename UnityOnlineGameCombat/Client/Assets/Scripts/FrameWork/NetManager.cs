7901d406b1df282ad1b8a57add316bcb834f6a20
Severn <1004005788@qq.com>
2020-05-07 23:02:52 +0800

    6.3 连接服务端

--- UnityOnlineGameCombat/Client/Assets/Scripts/FrameWork/NetManager.cs
+++ UnityOnlineGameCombat/Client/Assets/Scripts/FrameWork/NetManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
+using UnityEngine;
 
 namespace FrameWork
 {
@@ -23,6 +24,8 @@ namespace FrameWork
         private static Queue<ByteArray> writeQueue;
         // 事件监听列表
         static Dictionary<NetEvent,EventListener> eventListeners = new Dictionary<NetEvent, EventListener>();
+
+        static bool isConnecting = false;
 
         public static void AddEventListener(NetEvent netEvent, EventListener listener)
         {
@@ -47,6 +50,70 @@ namespace FrameWork
             {
                 eventListeners.Remove(netEvent);
             }
+        }
+
+        // 分发事件
+        public static void FireEvnet(NetEvent netEvent,String err)
+        {
+            if (eventListeners.ContainsKey(netEvent))
+            {
+                eventListeners[netEvent](err);
+            }
+        }
+        
+        public static void Connct(string ip,int port)
+        {
+            // 状态判断
+            if (socket !=null && socket.Connected)
+            {
+                Debug.Log("Connect fail already connected!");
+                return;
+            }
+            if (isConnecting)
+            {
+                Debug.Log("Connect fail isConnecting");
+                return;
+            }
+            // 初始化成员
+            InitState();
+            // 参数
+            socket.NoDelay = true;
+            // Connect
+            isConnecting = true;
+            socket.BeginConnect(ip, port, ConnectCallBack, socket);
+        }
+
+        private static void ConnectCallBack(IAsyncResult ar)
+        {
+            try
+            {
+                Socket socket = (Socket)ar.AsyncState;
+                socket.EndAccept(ar);
+                Debug.Log("Connect fail Succ ");
+                FireEvnet(NetEvent.ConnectSucc, "");
+                isConnecting = false;
+            }
+            catch (SocketException e)
+            {
+
+                Debug.Log("Connect fail " + e.ToString());
+                FireEvnet(NetEvent.ConnectFail, "");
+                isConnecting = false;
+            }
+        }
+        /// <summary>
+        /// 初始化状态
+        /// </summary>
+        private static void InitState()
+        {
+            // Socket
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // 缓冲区
+            readBuff = new ByteArray();
+            // 写入队列
+            writeQueue = new Queue<ByteArray>();
+            // 是否正在连接
+            isConnecting = true;
         }
     }
 }
