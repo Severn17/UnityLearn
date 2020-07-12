b6dd5faa4e5b8fe27a3926000cc1c2a7bfb36652
Severn <1004005788@qq.com>
2020-07-12 10:40:32 +0800

    10.11 玩家事件处理

--- UnityOnlineGameCombat/Server/Game/Game/logic/Room.cs
+++ UnityOnlineGameCombat/Server/Game/Game/logic/Room.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 public class Room
 {
@@ -12,4 +13,157 @@ public class Room
    public Dictionary<string,bool>playerIds = new Dictionary<string, bool>();
    public string owerId = "";
    public Status status = Status.PREPARE;
+
+   public bool AddPlayer(string id)
+   {
+      // 获取玩家
+      Player player = PlayerManager.GetPlayer(id);
+      if (player== null)
+      {
+         Console.WriteLine("room AddPlayer fail player is null");
+         return false;
+      }
+
+      if (playerIds.Count >= maxPlayer)
+      {
+         Console.WriteLine("room AddPlayer fail, reach maxPlayer");
+         return false;
+      }
+
+      if (status != Status.PREPARE)
+      {
+         Console.WriteLine("room AddPlayer fail, not PREPARE");
+         return false;
+      }
+
+      if (playerIds.ContainsKey(id))
+      {
+         Console.WriteLine("room AddPlayer fail, already in this room");
+         return false;
+      }
+
+      playerIds[id] = true;
+      // 设置玩家数据
+      player.camp = SwitchCamp();
+      player.roomId = this.id;
+      if (owerId == "")
+      {
+         owerId = player.id;
+      }
+
+      Broadcast(ToMsg());
+      return true;
+   }
+
+   public bool RemovePlayer(string id)
+   {
+      Player player = PlayerManager.GetPlayer(id);
+      if (player== null)
+      {
+         Console.WriteLine("room AddPlayer fail player is null");
+         return false;
+      }
+      if (!playerIds.ContainsKey(id))
+      {
+         Console.WriteLine("room AddPlayer fail, already in this room");
+         return false;
+      }
+
+      playerIds.Remove(id);
+      // 设置玩家数据
+      player.camp = 0;
+      player.roomId = -1;
+      // 设置房主
+      if (isOwner(player))
+      {
+         owerId = SwitchOwner();
+      }
+
+      if (playerIds.Count == 0)
+      {
+         RoomManager.RemoveRoom(this.id);
+      }
+      Broadcast(ToMsg());
+      return true;
+   }
+
+   private bool isOwner(Player player)
+   {
+      return player.id == owerId;
+   }
+
+   private string SwitchOwner()
+   {
+      foreach (string id in playerIds.Keys)
+      {
+         return id;
+      }
+      // 房间没人
+      return "";
+   }
+
+
+   private void Broadcast(MsgBase msg)
+   {
+      foreach (string id in playerIds.Keys)
+      {
+         Player player = PlayerManager.GetPlayer(id);
+         player.Send(msg);
+      }
+   }
+
+   public MsgBase ToMsg()
+   {
+      MsgGetRoomInfo msg = new MsgGetRoomInfo();
+      int count = playerIds.Count;
+      msg.players = new PlayerInfo[count];
+      // players
+      int i = 0;
+      foreach (string id in playerIds.Keys)
+      {
+         Player player = PlayerManager.GetPlayer(id);
+         PlayerInfo playerInfo = new PlayerInfo();
+         playerInfo.id = player.id;
+         playerInfo.camp = player.camp;
+         playerInfo.win = player.data.win;
+         playerInfo.lost = player.data.lost;
+         playerInfo.isOwner = 0;
+         if (isOwner(player))
+         {
+            playerInfo.isOwner = 1;
+         }
+
+         msg.players[i] = playerInfo;
+         i++;
+      }
+
+      return msg;
+   }
+
+   private int SwitchCamp()
+   {
+      int count1 = 0;
+      int count2 = 0;
+      foreach (var id in playerIds.Keys)
+      {
+         Player player = PlayerManager.GetPlayer(id);
+         if (player.camp == 1)
+         {
+            count1++;
+         }
+         if (player.camp == 2)
+         {
+            count2++;
+         }
+      }
+
+      if (count1 <= count2)
+      {
+         return 1;
+      }
+      else
+      {
+         return 2;
+      }
+   }
 }
--- UnityOnlineGameCombat/Server/Game/Game/logic/RoomManager.cs
+++ UnityOnlineGameCombat/Server/Game/Game/logic/RoomManager.cs
@@ -1,4 +1,52 @@
-﻿public class RoomManager
+﻿using System.Collections.Generic;
+
+public class RoomManager
 {
-        
+    //最大id
+    private static int maxId = 1;
+    public static Dictionary<int,Room> rooms = new Dictionary<int, Room>();
+
+    public static Room GetRoom(int id)
+    {
+        if (rooms.ContainsKey(id))
+        {
+            return rooms[id];
+        }
+
+        return null;
+    }
+
+    public static Room AddRoom()
+    {
+        maxId++;
+        Room room = new Room();
+        room.id = maxId;
+        rooms.Add(room.id,room);
+        return room;
+    }
+    
+    public static bool RemoveRoom(int id)
+    {
+        rooms.Remove(id);
+        return true;
+    }
+
+    public static MsgBase ToMsg()
+    {
+        MsgGetRoomList msg = new MsgGetRoomList();
+        int count = rooms.Count;
+        msg.rooms = new RoomInfo[count];
+        // players
+        int i = 0;
+        foreach (Room room in rooms.Values)
+        {
+            RoomInfo roomInfo = new RoomInfo();
+            roomInfo.id = room.id;
+            roomInfo.count = room.playerIds.Count;
+            roomInfo.status = (int) room.status;
+            msg.rooms[i] = roomInfo;
+            i++;
+        }
+        return msg;
+    }
 }
