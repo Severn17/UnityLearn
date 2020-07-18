1310be2af9943c7c47b84c49bb3c7b23265c2146
Severn <1004005788@qq.com>
2020-07-18 23:49:17 +0800

    打砖块Demo

---
+++ Unity2017ClassicGame/Assets/Breakout/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Breakout.Scripts
+{
+    public class Ball : MonoBehaviour
+    {
+        private Transform player;
+        private Rigidbody2D rb;
+        public float score;
+        private void Start()
+        {
+            player = transform.parent;
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void OnCollisionEnter2D(Collision2D collider)
+        {
+            if (collider.gameObject.name == "Block")
+            {
+                Destroy(collider.gameObject);
+                score += 5;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collider)
+        {
+            if (collider.CompareTag("Finish"))
+            {
+                transform.SetParent(player);
+                transform.position = player.position;
+                rb.velocity = Vector2.zero;
+            }
+        }
+    }
+    
+}
---
+++ Unity2017ClassicGame/Assets/Breakout/Scripts/BreakoutPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Breakout.Scripts
+{
+    public class BreakoutPlayer : MonoBehaviour
+    {
+        public float force;
+
+        public float moveSpeed;
+        public bool isStartGame;
+        public GameObject ball;
+
+        void Update()
+        {
+            if (!isStartGame)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    transform.DetachChildren();
+                    ball.GetComponent<Rigidbody2D>().AddForce(Vector2.up * force,ForceMode2D.Impulse);
+                }
+            }
+
+            float horizontalValue = Input.GetAxis("Horizontal");
+            transform.Translate(horizontalValue * moveSpeed * Time.deltaTime,0,0);
+        }
+    }
+}
+
+
