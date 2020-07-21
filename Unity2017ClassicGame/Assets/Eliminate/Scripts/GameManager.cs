5f15876b88c5bef90b601a3c8ccc2cb0f4255fd8
Severn <1004005788@qq.com>
2020-07-21 23:54:17 +0800

    任务9_06

--- Unity2017ClassicGame/Assets/Eliminate/Scripts/GameManager.cs
+++ Unity2017ClassicGame/Assets/Eliminate/Scripts/GameManager.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 namespace Eliminate.Scripts
 {
     public class GameManager : MonoBehaviour
     {
+        public enum SweetsType
+        {
+            EMPTY,
+            NOMAL,
+            BARRIER,
+            ROW_CLEAR,
+            COLUMN_CLEAR,
+            RAINBOWCANDY,
+            COUNT,//标记类型
+        }
+
+        public Dictionary<SweetsType, GameObject> sweetPrefabDic;
+        [Serializable]
+        public struct SweetsPrefab
+        {
+            public SweetsType type;
+            public GameObject prefab;
+        }
+
+        public SweetsPrefab[] sweetsPrefabs;
+        
         private static GameManager _instance;
         public static GameManager Instance
         {
@@ -16,6 +39,8 @@ namespace Eliminate.Scripts
         public int yRow;
         public GameObject gridPrefab;
 
+        private GameObject[,] sweets;
+
         private void Awake()
         {
             _instance = this;
@@ -23,12 +48,31 @@ namespace Eliminate.Scripts
 
         private void Start()
         {
+            sweetPrefabDic = new Dictionary<SweetsType, GameObject>();
+            for (int i = 0; i < sweetsPrefabs.Length; i++)
+            {
+                if (!sweetPrefabDic.ContainsKey(sweetsPrefabs[i].type))
+                {
+                    sweetPrefabDic.Add(sweetsPrefabs[i].type,sweetsPrefabs[i].prefab);
+                }
+            }
+            
             for (int x = 0; x < xColumn; x++)
             {
                 for (int y = 0; y < yRow; y++)
                 {
                     GameObject chocolate = Instantiate(gridPrefab, CorrectPosition(x, y), Quaternion.identity);
                     chocolate.transform.SetParent(transform);
+                }
+            }
+            
+            sweets = new GameObject[xColumn,yRow];
+            for (int x = 0; x < xColumn; x++)
+            {
+                for (int y = 0; y < yRow; y++)
+                {
+                    sweets[x,y] = Instantiate(sweetPrefabDic[SweetsType.NOMAL], CorrectPosition(x, y), Quaternion.identity);
+                    sweets[x,y].transform.SetParent(transform);
                 }
             }
         }
---
+++ Unity2017ClassicGame/Assets/Eliminate/Scripts/GameSweet.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Eliminate.Scripts
+{
+    public class GameSweet : MonoBehaviour
+    {
+        private int x;
+
+        public int X
+        {
+            get => x;
+            set => x = value;
+        }
+
+        private int y;
+
+        public int Y
+        {
+            get => y;
+            set => y = value;
+        }
+
+        private GameManager.SweetsType type;
+
+        public GameManager.SweetsType Tyep
+        {
+            get => type;
+        }
+
+        public GameManager gameManager;
+
+        public void Init(int x,int y,GameManager gameManager,GameManager.SweetsType type)
+        {
+            this.x = x;
+            this.y = y;
+            this.gameManager = gameManager;
+            this.type = type;
+        }
+        
+    }
+}
